dc0fd217d3528974d775f8f57a062157a22d0eea
Abdoulaye <abdoulaye.sedego95@gmail.com>
2022-04-19 07:22:31 -0400

    project set up

---
+++ projects/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
+
+def projects(request):
+    msg = 'Hello, you are on the Home page'
+    number = 12
+    context = {'message': msg, 'number': number, 'projects': projectsList}
+
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})

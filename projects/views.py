4af73b3897bace6fa69feda0824ed96b05144753
Abdoulaye <abdoulaye.sedego95@gmail.com>
2022-04-21 05:48:45 -0400

    data model created

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Project
 
 
 projectsList = [
@@ -24,16 +25,13 @@ projectsList = [
 
 
 def projects(request):
-    msg = 'Hello, you are on the Home page'
-    number = 12
-    context = {'message': msg, 'number': number, 'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
 
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'tags': tags})

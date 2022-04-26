7558e35f3e9b8f7e5d384e4f4628af47eac470e5
Abdoulaye <abdoulaye.sedego95@gmail.com>
2022-04-25 23:55:14 -0400

    static files directories setup

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/mediafiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

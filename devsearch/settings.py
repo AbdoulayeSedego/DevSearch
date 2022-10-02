dbac6a6b27e645894d654e3437d404657b04c20e
Abdoulaye <abdoulaye.sedego95@gmail.com>
2022-10-02 06:40:19 -0400

    add profiles routes and template

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # Installed apps
     'projects.apps.ProjectsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL  = 'staticfiles/'
+MEDIA_URL = 'staticfiles/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
